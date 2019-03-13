ac85b9af4bddbf2a9d7325fc04d94545016efe07
João Lima <figorifico3000@gmail.com>
2019-03-13 09:52:24 +0000

    operators

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,18 +1,88 @@
 #include "Robot.h"
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 
 using namespace std;
 
 Robot::Robot(char type) : type(type), x(-1), y(-1) {}
 
+
+
+
+std::pair<int,int> Robot::MoveTop(vector<vector<char>> map, std::pair<int,int> currentPos)
+{
+    int currentX=currentPos.first;
+    int currentY=currentPos.second;
+    while(currentX>0 && currentX<=map.size() && currentY>0 && currentY<=map[0].size())
+    {
+        if(map[currentX][currentY]!= ' ')
+        {
+            return make_pair(currentX,currentY);
+        }
+        else
+        {
+            currentX--;
+        }
+    }
+    return make_pair(currentX,currentY);
+}
+std::pair<int,int> Robot::MoveBottom(vector<vector<char>> map,std::pair<int,int> currentPos)
+{
+    int currentX=currentPos.first;
+    int currentY=currentPos.second;
+    while(currentX>0 && currentX<=map.size() && currentY>0 && currentY<=map[0].size())
+    {
+        if(map[currentX][currentY]!= ' ')
+        {
+            return make_pair(currentX,currentY);
+        }
+        else
+        {
+            currentX++;
+        }
+    }
+    return make_pair(currentX,currentY);
+}
+std::pair<int,int> Robot::MoveLeft(vector<vector<char>> map,std::pair<int,int> currentPos)
+{
+    int currentX=currentPos.first;
+    int currentY=currentPos.second;
+    while(currentX>0 && currentX<=map.size() && currentY>0 && currentY<=map[0].size())
+    {
+        if(map[currentX][currentY]!= ' ')
+        {
+            return make_pair(currentX,currentY);
+        }
+        else
+        {
+            currentY--;
+        }
+    }
+    return make_pair(currentX,currentY);
+}
+std::pair<int,int> Robot::MoveRight(vector<vector<char>> map,std::pair<int,int> currentPos)
+{
+    int currentX=currentPos.first;
+    int currentY=currentPos.second;
+    while(currentX>0 && currentX<=map.size() && currentY>0 && currentY<=map[0].size())
+    {
+        if(map[currentX][currentY]!= ' ')
+        {
+            return make_pair(currentX,currentY);
+        }
+        else
+        {
+            currentY++;
+        }
+    }
+    return make_pair(currentX,currentY);
+}
+
+
 bool Robot::move(char direction, vector<vector<char>> map)
 {
     int xVector = 0;
     int yVector = 0;
-  
-    
-  
 
     switch (direction)
     {
@@ -33,25 +103,21 @@ bool Robot::move(char direction, vector<vector<char>> map)
         return false;
     }
 
-    
+    while (map[this->getY() + yVector][this->getX() + xVector] == ' ')
+    {
 
-    while(map[this->getY()+yVector][this->getX()+xVector] == ' '){
-
- this->x += xVector;
+        this->x += xVector;
         this->y += yVector;
-       
-
     }
 
-      if(toupper(map[this->getY()+yVector][this->getX()+xVector]) == this->getType()){
+    if (toupper(map[this->getY() + yVector][this->getX() + xVector]) == this->getType())
+    {
 
-           this->x += xVector;
+        this->x += xVector;
         this->y += yVector;
-    map[this->getY()+yVector][this->getX()+xVector] = this->getType();
-  
+        map[this->getY() + yVector][this->getX() + xVector] = this->getType();
     }
     return true;
-
 }
 
 void Robot::setPosition(int x, int y)
@@ -70,6 +136,7 @@ int Robot::getY()
     return this->y;
 }
 
-char Robot::getType(){
+char Robot::getType()
+{
     return this->type;
 }
--- src/Robot.h
+++ src/Robot.h
@@ -8,6 +8,12 @@ class Robot {
 private: 
     char type;
     int x,y;
+
+    std::pair<int,int> MoveTop(std::vector<std::vector<char>> map,std::pair<int,int> currentPos);
+    std::pair<int,int> MoveBottom(std::vector<std::vector<char>> map,std::pair<int,int> currentPos);
+    std::pair<int,int> MoveRight(std::vector<std::vector<char>> map,std::pair<int,int> currentPos);
+    std::pair<int,int> MoveLeft(std::vector<std::vector<char>> map,std::pair<int,int> currentPos);
+
 public:
     Robot(char type);
     bool move(char direction, std::vector<std::vector<char>> map);
