7c0034f2377d4e4b1cc36228de672fa5053fa330
João Augusto Lima <figorifico3000@gmail.com>
2019-03-21 21:22:34 +0000

    fixed bug

--- src/AI.cpp
+++ src/AI.cpp
@@ -11,34 +11,4 @@ bool AI::makeMove(char robot,char direction)
     return true;
 }
 
-/*
-std::string BotPlayer::play(){
 
-    string line;
-
-    srand(time(NULL));   // Initialization, should only be called once.
-
-    int robot = rand()%2;
-    int move = rand()%4;
-
-    if(robot == 0)
-    line = "A";
-    else line = "L";
-
-    switch(move){
-        case 0: 
-        line += "u";
-        break;
-        case 1:
-        line += "d";
-        break;
-        case 2:
-        line += "l";
-        break;
-        case 3:
-        line += "r";
-        break;
-    }
-    cin.ignore();
-    return line;
-}*/
